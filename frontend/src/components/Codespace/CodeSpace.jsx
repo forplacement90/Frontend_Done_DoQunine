94efd381809e9540a06da99d82241c700814468a
sourav kumar <souravkumar85055@gmail.com>
2025-03-22 13:33:32 +0530

    ll

--- frontend/src/components/Codespace/CodeSpace.jsx
+++ frontend/src/components/Codespace/CodeSpace.jsx
@@ -17,7 +17,7 @@ export default function CodeSpace() {
       const formattedOutput = `
         <html>
           <head>
-            <script src="https://cdn.tailwindcss.com"></script>
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
             <style>body { font-family: sans-serif; padding: 10px; }</style>
           </head>
           <body>${code}</body>
